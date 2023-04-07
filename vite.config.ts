a13fbabec9cc98831b8ef971d9d98d6b05880c1a
infected-by-js <infectedbyjs@gmail.com>
2023-04-07 15:33:55 +1000

    Fix vite.config.ts
    - add node types

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import {resolve} from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
 });
