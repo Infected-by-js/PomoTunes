794d890fdb49159cc990aaf1d3f7983ae365e497
infected-by-js <infectedbyjs@gmail.com>
2023-04-13 15:10:56 +1000

    Create shared Sliders

---
+++ src/shared/components/Slider.tsx
@@ -0,0 +1,45 @@
+import {ChangeEvent, FC, useState} from 'react';
+import {generateBackgroundGradient} from '@/shared/utils/helpers';
+
+interface Props {
+  value: number;
+  min?: number;
+  max?: number;
+  color?: string;
+  onChange: (value: number) => void;
+  isDisabled?: boolean;
+}
+
+const Slider: FC<Props> = (props) => {
+  const {color, min = 1, max = 90, value, onChange, isDisabled} = props;
+  const [range, setRange] = useState(value);
+
+  const generateBackground = () => {
+    if (isDisabled) return 'rgba(0,0,0,0.1)';
+
+    const bgColor = color ? color : 'var(--color-accent-500)';
+
+    return generateBackgroundGradient({color: bgColor, value: range, min, max});
+  };
+
+  const updateSlider = (e: ChangeEvent<HTMLInputElement>) => {
+    setRange(Number(e.target.value));
+    onChange(Number(e.target.value));
+  };
+
+  return (
+    <input
+      className="slider"
+      type="range"
+      step="1"
+      disabled={isDisabled}
+      min={min}
+      max={max}
+      value={range}
+      style={{background: generateBackground()}}
+      onChange={updateSlider}
+    />
+  );
+};
+
+export default Slider;
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,3 +2,5 @@ export {default as Keymap} from './Keymap';
 export {default as PageContainer} from './PageContainer';
 export {default as ProgressLinear} from './ProgressLinear';
 export {default as Checkbox} from './Checkbox';
+export {default as Slider} from './Slider';
+export {default as SliderWithLabel} from './SliderWithLabel';
