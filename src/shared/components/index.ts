641e8fd18fb7b10ccc0f56435109bbd724cf87cc
infected-by-js <infectedbyjs@gmail.com>
2023-04-13 15:11:19 +1000

    Create shared Input

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,5 +2,6 @@ export {default as Keymap} from './Keymap';
 export {default as PageContainer} from './PageContainer';
 export {default as ProgressLinear} from './ProgressLinear';
 export {default as Checkbox} from './Checkbox';
+export {default as Input} from './Input';
 export {default as Slider} from './Slider';
 export {default as SliderWithLabel} from './SliderWithLabel';
