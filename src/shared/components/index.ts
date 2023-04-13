1a6fea434a111191579a30dc6c4dda41ec31489e
infected-by-js <infectedbyjs@gmail.com>
2023-04-13 15:11:33 +1000

    Create shared Divider

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,6 +1,7 @@
 export {default as Keymap} from './Keymap';
 export {default as PageContainer} from './PageContainer';
 export {default as ProgressLinear} from './ProgressLinear';
+export {default as DividerHorizontal} from './DividerHorizontal';
 export {default as Checkbox} from './Checkbox';
 export {default as Input} from './Input';
 export {default as Slider} from './Slider';
