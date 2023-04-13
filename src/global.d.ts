b4289bd9b2581a84d134c78398754d1818a694e3
infected-by-js <infectedbyjs@gmail.com>
2023-04-13 15:10:02 +1000

    Create shared Checkbox

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,3 +3,9 @@ export {};
 declare global {
   type TimerMode = 'focus' | 'short_break' | 'long_break';
 }
+
+declare module 'react' {
+  type CssVariableInStyles = Record<`--${string}`, string>;
+
+  export interface CSSProperties extends CssVariableInStyles {}
+}
--- src/shared/components/Checkbox.tsx
+++ src/shared/components/Checkbox.tsx
@@ -1,24 +1,31 @@
-import {FC, useState} from 'react';
+import {ChangeEvent, FC, useState} from 'react';
 import clsx from 'clsx';
-import './Checkbox.css';
 
 interface Props {
   value: boolean;
+  color?: string;
   onChange: (isChecked: boolean) => void;
   isDisabled?: boolean;
 }
 
-const Checkbox: FC<Props> = ({value, onChange, isDisabled = false}) => {
+const Checkbox: FC<Props> = (props) => {
+  const {value, onChange, color = 'var(--color-accent-500)', isDisabled = false} = props;
   const [isChecked, setIsChecked] = useState(value);
 
-  const handleInputChange = (e) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.checked;
     setIsChecked(newValue);
     onChange(newValue);
   };
 
   return (
-    <label className={clsx('checkbox', {checked: isChecked, disabled: isDisabled})}>
+    <label
+      className={clsx('checkbox', {
+        checked: isChecked,
+        disabled: isDisabled,
+      })}
+      style={{'--color-checkbox': color}}
+    >
       <input
         onChange={handleInputChange}
         disabled={isDisabled}
